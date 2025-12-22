160362dffd9fcce2096a1e20929bbd090b8ee092
[dyad] <sadok.zgolli@gmail.com>
2025-12-22 16:38:03 +0100

    [dyad] wrote 1 file(s)

--- src/components/References.tsx
+++ src/components/References.tsx
@@ -78,7 +78,8 @@ const References = () => {
           <div className="flex justify-center items-center py-20">
             <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-primary"></div>
           </div>
-        </section>
+        </div> {/* Added missing closing div here */}
+      </section>
     );
   }
 
