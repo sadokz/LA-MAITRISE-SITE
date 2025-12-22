e1a936973a398600e6020b8a123008c41179992c
[dyad] <sadok.zgolli@gmail.com>
2025-12-22 22:59:59 +0100

    [dyad] wrote 1 file(s)

--- src/pages/RealisationsPage.tsx
+++ src/pages/RealisationsPage.tsx
@@ -1,4 +1,3 @@
-' button after the hero section on the Realisations page.">
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import Header from '@/components/Header';
